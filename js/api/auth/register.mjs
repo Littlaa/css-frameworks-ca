755177806b59d54c6c7a90297adb8a69859de8a5
Littlaa <monadags@gmail.com>
2022-10-12 16:08:10 +0200

    log in works

--- js/api/auth/register.mjs
+++ js/api/auth/register.mjs
@@ -16,7 +16,6 @@ export async function register(profile) {
   });
 
   const result = await response.json();
-  console.log(result);
+  alert("You are now registered");
+  return result;
 }
-
-// 50 min https://www.youtube.com/watch?v=rLAGHFr8bvU
--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,10 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
 
-setRegisterFormListener();
+const path = location.pathname;
+
+if (path === "/login.html") {
+  setLoginFormListener();
+} else if (path === "/register.html") {
+  setRegisterFormListener();
+}
